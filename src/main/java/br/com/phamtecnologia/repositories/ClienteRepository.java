7092572e730e6ce405857d870a0bb1779130cd61
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2024-09-08 20:17:56 -0300

    feature: Método create no repository.

--- src/main/java/br/com/phamtecnologia/repositories/ClienteRepository.java
+++ src/main/java/br/com/phamtecnologia/repositories/ClienteRepository.java
@@ -1,13 +1,30 @@
 package main.java.br.com.phamtecnologia.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 import main.java.br.com.phamtecnologia.entities.Cliente;
+import main.java.br.com.phamtecnologia.factories.ConnectionFactory;
 
 public class ClienteRepository {
 
     public void create(Cliente cliente) throws Exception {
-        //  TODO
+
+        Connection connection = ConnectionFactory.getConnection();
+        
+        String query = "INSERT INTO CLIENTE(nome, email, cpf, telefone, observacoes) VALUES(?,?,?,?,?)";
+        
+        PreparedStatement statement = connection.PreparedStatement(query);
+        statement.setString(1, cliente.getNome());
+        statement.setString(2, cliente.getEmail());
+        statement.setString(3, cliente.getCpf());
+        statement.setString(4, cliente.getTelefone());
+        statement.setString(5, cliente.getObservacoes());
+        statement.executeUpdate();
+
+        statement.close();
+
     }
 
     public void update(Cliente cliente) throws Exception {
