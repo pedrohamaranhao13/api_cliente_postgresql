7082256286e1d3555e922150f6637d42e5773886
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2024-09-08 18:27:07 -0300

    first commit

---
+++ src/main/java/br/com/phamtecnologia/repositories/ClienteRepository.java
@@ -0,0 +1,30 @@
+package main.java.br.com.phamtecnologia.repositories;
+
+import java.util.List;
+
+import main.java.br.com.phamtecnologia.entities.Cliente;
+
+public class ClienteRepository {
+
+    public void create(Cliente cliente) throws Exception {
+        //  TODO
+    }
+
+    public void update(Cliente cliente) throws Exception {
+        //  TODO
+    }
+
+    public void delete(Cliente cliente) throws Exception {
+        //  TODO
+    }
+
+    public List<Cliente> findALL() throws Exception {
+        //  TODO
+        return null;
+    }
+
+    public Cliente findById(Integer idCliente) throws Exception {
+        //  TODO
+        return null;
+    }
+}
